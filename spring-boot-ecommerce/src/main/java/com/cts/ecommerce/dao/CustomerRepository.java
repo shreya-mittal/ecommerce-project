2c45cbbcee3d5a6f6968be011265799399fb4123
Shreya Mittal <64227764+shreya-mittal@users.noreply.github.com>
2022-10-19 21:49:29 +0530

    Delete spring-boot-ecommerce directory

--- spring-boot-ecommerce/src/main/java/com/cts/ecommerce/dao/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cts.ecommerce.dao;
-
-import com.cts.ecommerce.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-	Customer findByEmail(String theEmail);
-}
--- spring-boot-ecommerce/src/main/java/com/cts/ecommerce/service/CheckoutService.java
+++
@@ -1,9 +0,0 @@
-package com.cts.ecommerce.service;
-
-import com.cts.ecommerce.dto.Purchase;
-import com.cts.ecommerce.dto.PurchaseResponse;
-
-public interface CheckoutService {
-
-    PurchaseResponse placeOrder(Purchase purchase);
-}
